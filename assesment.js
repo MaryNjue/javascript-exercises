d3fa42f45bea85c59f54b07289800e7cc70ee532
Mary <njuemarywanjiru@gmail.com>
2024-09-15 17:57:17 +0300

    adds the javascript file

---
+++ assesment.js
@@ -0,0 +1,17 @@
+//number1//
+function addition (num1, num2){
+    return num1 + num2
+}
+let result = addition(5,6)
+console.log(result)
+//number2//
+let min = 5;
+function convert(){
+   return min * 60
+}
+let seconds = convert()
+console.log(seconds)
+
+//number3//
+for (let i=1) , i<=1,i=<i></i>
+<1></1>
